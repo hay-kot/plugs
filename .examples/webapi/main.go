ba283bf7a47e199c88a8a73d467c0caa083d4ee4
Hayden <64056131+hay-kot@users.noreply.github.com>
2024-09-08 16:27:04 -0500

    new web api example

---
+++ .examples/webapi/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"syscall"
+
+	"github.com/hay-kot/plugs/plugs"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	mgr := plugs.New(
+		plugs.WithPrintln(log.Println),
+		plugs.WithRestart(3),
+		plugs.WithSignals(os.Interrupt, syscall.SIGTERM),
+	)
+
+	mgr.Add(
+		&server{port: 9091},
+		&server{port: 9082},
+		&server{port: 9083},
+	)
+
+	return mgr.Start(context.Background())
+}
+
+type server struct {
+	port int
+}
+
+func (s *server) Name() string {
+	return fmt.Sprintf("server-%d", s.port)
+}
+
+func (s *server) Start(ctx context.Context) error {
+	shutdown := make(chan struct{})
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
+		close(shutdown)
+	})
+
+	mux.HandleFunc("/panic", func(w http.ResponseWriter, r *http.Request) {
+		panic("server panic")
+	})
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", s.port),
+		Handler: mux,
+	}
+
+	go func() {
+		select {
+		case <-shutdown:
+		case <-ctx.Done():
+		}
+
+		_ = server.Shutdown(context.Background())
+	}()
+
+	log.Printf("server-%d started", s.port)
+	err := server.ListenAndServe()
+	log.Printf("server-%d stopped", s.port)
+	return err
+}
